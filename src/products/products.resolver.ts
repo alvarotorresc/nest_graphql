2fcef26ae076d388fe2fb91b6345ec7883efa5a3
alvarotorresc <alvarotorrescarrasco@gmail.com>
2020-03-19 21:30:02 +0100

    feat(products): products service

--- src/products/products.resolver.ts
+++ src/products/products.resolver.ts
@@ -1,4 +1,14 @@
 import { Resolver } from '@nestjs/graphql';
+import { ProductsService } from './products.service';
+import { Query } from '@nestjs/graphql';
+import { ProductType } from './dto/create-product.dto';
 
 @Resolver('Products')
-export class ProductsResolver {}
+export class ProductsResolver {
+    constructor(private readonly productsService: ProductsService) { }
+
+    @Query(() => [ProductType])
+    async products(): Promise<ProductType[]> {
+        return this.productsService.findAll();
+    }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from './interfaces/product.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { ProductType } from './dto/create-product.dto';
+import { ProductInput } from './input-products.product';
+
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor(@InjectModel('Product') private productModel: Model<Product>) { }
+
+    async findAll(): Promise<ProductType[]> {
+        return await this.productModel.find().exec();
+    }
+
+    async findOne(id: string): Promise<ProductType> {
+        return await this.productModel.findOne({ _id: id });
+    }
+
+    async createProduct(createProductDto: ProductInput): Promise<ProductType> {
+        const createProduct = new this.productModel(createProductDto);
+        return await createProduct.save();
+    }
+
+    async delete(id: string): Promise<ProductType> {
+        return await this.productModel.findByIdAndRemove(id);
+    }
+
+    async update(id: string, product: Product): Promise<ProductType> {
+        return await this.productModel.findByIdAndUpdate(id, product, { new: true });
+    }
+}
